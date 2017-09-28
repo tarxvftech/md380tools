33237a48f564aa93719a7648a4bf98acef8f0069
mike mcginty <mach327@gmail.com>
2017-09-27 22:29:44 -0400

    some more of KG5RKI's MD2017 work, including parts from issue 789

---
+++ applet/merge_d3.31.py
@@ -0,0 +1,315 @@
+#! python2.7
+# -*- coding: utf-8 -*-
+
+
+# This script implements our old methods for merging an MD380 firmware
+# image with its patches.  It is presently being rewritten to require
+# fewer explicit addresses, so that we can target our patches to more
+# than one version of the MD380 firmware.
+
+from __future__ import print_function
+
+import sys
+
+
+class Symbols(object):
+    addresses = {}
+    names = {}
+
+    def __init__(self, filename):
+        print("Loading symbols from %s" % filename)
+        fsyms = open(filename)
+        for l in fsyms:
+            try:
+                r = l.strip().split('\t')
+                if len(r) == 2 and r[0].split(' ')[7] == '.text':
+                    adr = r[0].split(' ')[0].strip()
+                    name = r[1].split(' ')[1]  # .strip();
+                    # print("%s is at %s" % (name, adr))
+                    self.addresses[name] = int(adr, 16)
+                    self.names[int(adr, 16)] = name
+            except IndexError:
+                pass;
+    def getadr(self,name):
+        return self.addresses[name];
+    def try_getadr(self,name): # DL4YHF 2017-01, used to CHECK if a symbol exists
+        try:                   # to perform patches for 'optional' C functions 
+            return self.addresses[name];
+        except KeyError:
+            return None;
+    def getname(self,adr):
+        return self.names[adr];
+
+class Merger(object):
+    def __init__(self, filename, offset=0x0800C000):
+        """Opens the input file."""
+        self.offset = offset
+        self.file = open(filename, "rb")
+        self.bytes = bytearray(self.file.read())
+        self.length = len(self.bytes)
+
+    def setbyte(self, adr, new, old=None):
+        """Patches a single byte from the old value to the new value."""
+        self.bytes[adr - self.offset] = new
+
+    def getbyte(self, adr):
+        """Reads a byte from the firmware address."""
+        b = self.bytes[adr - self.offset]
+        return b
+
+    def export(self, filename):
+        """Exports to a binary file."""
+        outfile = open(filename, "wb")
+        outfile.write(self.bytes)
+
+    def assertbyte(self, adr, val):
+        """Asserts that a byte has a given value."""
+        assert self.getbyte(adr) == val
+        return
+
+    def getword(self, adr):
+        """Reads a byte from the firmware address."""
+        w = (
+            self.bytes[adr - self.offset] +
+            (self.bytes[adr - self.offset + 1] << 8) +
+            (self.bytes[adr - self.offset + 2] << 16) +
+            (self.bytes[adr - self.offset + 3] << 24)
+        )
+
+        return w
+
+    def setword(self, adr, new, old=None):
+        """Patches a 32-bit word from the old value to the new value."""
+        if old is not None:
+            self.assertbyte(adr, old & 0xFF)
+            self.assertbyte(adr + 1, (old >> 8) & 0xFF)
+            self.assertbyte(adr + 2, (old >> 16) & 0xFF)
+            self.assertbyte(adr + 3, (old >> 24) & 0xFF)
+
+        # print("Patching word at %08x to %08x" % (adr, new))
+        self.bytes[adr - self.offset] = new & 0xFF
+        self.bytes[adr - self.offset + 1] = (new >> 8) & 0xFF
+        self.bytes[adr - self.offset + 2] = (new >> 16) & 0xFF
+        self.bytes[adr - self.offset + 3] = (new >> 24) & 0xFF
+        self.assertbyte(adr, new & 0xFF)
+        self.assertbyte(adr + 1, (new >> 8) & 0xFF)
+
+    def sethword(self, adr, new, old=None):
+        """Patches a byte pair from the old value to the new value."""
+        if old is not None:
+            self.assertbyte(adr, old & 0xFF)
+            self.assertbyte(adr + 1, (old >> 8) & 0xFF)
+        # print("Patching hword at %08x to %04x" % (adr, new))
+        self.bytes[adr - self.offset] = new & 0xFF
+        self.bytes[adr - self.offset + 1] = (new >> 8) & 0xFF
+        self.assertbyte(adr, new & 0xFF)
+        self.assertbyte(adr + 1, (new >> 8) & 0xFF)
+
+    def hookstub(self, adr, handler):
+        """Hooks a function by placing an unconditional branch at adr to
+           handler.  The recipient function must have an identical calling
+           convention. """
+        adr &= ~1  # Address must be even.
+        handler |= 1  # Destination address must be odd.
+        # print("Inserting a stub hook at %08x to %08x." % (adr, handler))
+
+        # FIXME This clobbers r0, should use a different register.
+        self.sethword(adr, 0x4801)  # ldr r0, [pc, 4]
+        self.sethword(adr + 2, 0x4700)  # bx r0
+        self.sethword(adr + 4, 0x4600)  # NOP
+        self.sethword(adr + 6, 0x4600)  # NOP, might be overwritten
+        if adr & 2 > 0:
+            self.setword(adr + 6, handler)  # bx r0
+        else:
+            self.setword(adr + 8, handler)  # bx r0
+
+    def hookstub2(self, adr, handler):
+        """Hooks a function by placing an unconditional branch at adr to
+           handler.  The recipient function must have an identical calling
+           convention. """
+        adr &= ~1  # Address must be even.
+        handler |= 1  # Destination address must be odd.
+        print("Inserting a stub hook at %08x to %08x." % (adr, handler))
+
+        # insert trampoline
+        # rasm2 -a arm -b 16 '<asm code>'
+        self.sethword(adr, 0xb401)  # push {r0}
+        self.sethword(adr + 2, 0xb401)  # push {r0}
+        self.sethword(adr + 4, 0x4801)  # ldr r0, [pc, 4]
+        self.sethword(adr + 6, 0x9001)  # str r0, [sp, 4] (pc)
+        self.sethword(adr + 8, 0xbd01)  # pop {r0,pc}
+        self.sethword(adr + 10, 0x4600)  # NOP, might be overwritten
+        if adr & 2 > 0:
+            self.setword(adr + 10, handler)
+        else:
+            self.setword(adr + 12, handler)
+
+    def calcbl(self, adr, target):
+        """Calculates the Thumb code to branch to a target."""
+        offset = target - adr
+        # print("offset=%08x" % offset)
+        offset -= 4  # PC points to the next ins.
+        offset = (offset >> 1)  # LSBit is ignored.
+        hi = 0xF000 | ((offset & 0xfff800) >> 11)  # Hi address setter, but at lower adr.
+        lo = 0xF800 | (offset & 0x7ff)  # Low adr setter goes next.
+        # print("%04x %04x" % (hi, lo))
+        word = ((lo << 16) | hi)
+        # print("%08x" % word)
+        return word
+
+    def hookbl(self, adr, handler, oldhandler=None):
+        """Hooks a function by replacing a 32-bit relative BL."""
+
+        # print("Redirecting a bl at %08x to %08x." % (adr, handler))
+
+        # TODO This is sometimes tricked by old data.
+        # Fix it by ensuring no old data.
+        # if oldhandler!=None:
+        #    #Verify the old handler.
+        #    if self.calcbl(adr,oldhandler)!=self.getword(adr):
+        #        print("The old handler looks wrong.")
+        #        print("Damn, we're in a tight spot!")
+        #        sys.exit(1);
+
+        self.setword(adr,
+                     self.calcbl(adr, handler))
+
+
+if __name__ == '__main__':
+    print("Merging an applet.")
+    if len(sys.argv) != 4:
+        print("Usage: python merge.py firmware.img patch.img offset")
+        sys.exit(1)
+
+    # Open the firmware image.
+    merger = Merger(sys.argv[1])
+
+    # Open the applet.
+    fapplet = open(sys.argv[2], "rb")
+    bapplet = bytearray(fapplet.read())
+    index = int(sys.argv[3], 16)
+
+    # Open the applet symbols
+    sapplet = Symbols("%s.sym" % sys.argv[2])
+	
+    #merger.hookstub(0x080E50C6,  # USB manufacturer string handler function.
+    #                sapplet.getadr("getmfgstr"))
+	
+    merger.hookbl(0x08030D76, sapplet.getadr("rx_screen_blue_hook"), 0)
+    merger.hookbl(0x08030DF0, sapplet.getadr("rx_screen_blue_hook"), 0)
+	
+    #merger.hookbl(0x080D8A20, sapplet.getadr("usb_upld_hook"), 0x080D94BC)  # Old handler adr.
+	# keyboard
+    merger.hookbl(0x0806CE92, sapplet.getadr("kb_handler_hook"));
+
+    dmr_call_start_hook_list = [0x804C3DC, 0x0804BE34, 0x804BE0C, 0x804BDA6]
+    for adr in dmr_call_start_hook_list:
+        merger.hookbl(adr, sapplet.getadr("dmr_call_start_hook"))
+
+    gfxblockfill = [
+        0x0800CAA6,
+        0x0800CAB2,
+        0x0800CABE,
+        0x0800CACA,
+        0x0800CADE,
+        0x0800CAEA,
+        0x0800CAF6,
+        0x0800CB02,
+        0x0800CB0E,
+        0x0800CB28,
+        0x0800CB4E,
+        0x0800CB5A,
+        0x0800CB6E,
+        0x0800CC4E,
+        0x0800CC5A,
+        0x0800CC66,
+        0x0800CC78,
+        0x0800CC96,
+        0x0800CCA2,
+        0x0800CCB6,
+        0x0800CD4C,
+        0x0800CD58,
+        0x0800CD64,
+        0x0800CDDC,
+        0x0800CDE8,
+        0x0800CDF4,
+        0x0800CE00,
+        0x0800CE14,
+        0x0800CE20,
+        0x0800CE34,
+        0x0800CE40,
+        0x0800CE54,
+        0x0800CECA,
+        0x0800D258,
+        0x0800D28A,
+        0x0800D450,
+        0x0800D45C,
+        0x0800D4C0,
+        0x0800D5EC,
+        0x0800D5F8,
+        0x0800D604,
+        0x0800D610,
+        0x0800D722,
+        0x0800D72E,
+        0x0800D73A,
+        0x0800D746,
+        0x0800D7FA,
+        0x0800D87A,
+        0x0800D886,
+        0x0800D892,
+        0x0800D89E,
+        0x0800DB3A,
+        0x0800DC82,
+        0x0800DC9C,
+        0x0800DCA8,
+        0x0800DCF8,
+        0x0800DD04,
+        0x0800DD10,
+        0x0800DFF2,
+        0x0800E27E,
+        0x0800E292,
+        0x0800E29E,
+        0x0800E2AA,
+        0x0800E2B6,
+        0x0800E558,
+        0x0800E5CE,
+        0x0800E5DC,
+        0x0800E5E8,
+        0x0800E5F4,
+        0x0800E608,
+        0x0800E6E2,
+        0x0800E74C,
+        0x0800E774,
+        0x0800E90C,
+        0x0800E920,
+        0x0800E92C,
+        0x0800E938,
+        0x0800E944,
+        0x0800ECBE,
+        0x0800ECCC,
+        0x0800EE84,
+        0x0800EF9C,
+        0x0800F074,
+        0x0800F0DE,
+        0x0800F0EC,
+        0x0800F1E4,
+        0x0800F2CE,
+        0x0800F3C6,
+        0x0800F702,
+        0x0800F71C,
+        0x0800F75E
+    ]
+    for adr in gfxblockfill:
+        merger.hookbl(adr, sapplet.getadr("gfx_blockfill_hook"))
+    
+    print("Merging %s into %s at %08x" % (
+        sys.argv[2],
+        sys.argv[1],
+        index))
+
+    i = 0
+    for b in bapplet:
+        merger.setbyte(index + i, bapplet[i])
+        i += 1
+
+    merger.export(sys.argv[1])
